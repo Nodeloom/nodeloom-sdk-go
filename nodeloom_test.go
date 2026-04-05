90a9d80e776596eaf7784013c6de44bcf10c79aa
Reda Zerrad <reda@nodeloom.io>
2026-04-04 23:26:51 -0700

    Bump SDK version 0.7.0 to 0.8.0

--- nodeloom_test.go
+++ nodeloom_test.go
@@ -153,8 +153,8 @@ func TestClient_FlushAfterClose(t *testing.T) {
 }
 
 func TestSDKConstants(t *testing.T) {
-	if SDKVersion != "0.7.0" {
-		t.Errorf("expected SDK version '0.7.0', got %q", SDKVersion)
+	if SDKVersion != "0.8.0" {
+		t.Errorf("expected SDK version '0.8.0', got %q", SDKVersion)
 	}
 	if SDKLanguage != "go" {
 		t.Errorf("expected SDK language 'go', got %q", SDKLanguage)
